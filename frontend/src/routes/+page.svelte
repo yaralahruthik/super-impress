f3a3055a84614be2ea64f27133025ab6e62e2cb1
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-10-10 16:57:44 +0530

    chore: add Docker support and initial frontend/backend setup

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const API_BASE = import.meta.env?.VITE_API_BASE;
+	let data: { message: string } | null = null;
+
+	onMount(async () => {
+		try {
+			const res = await fetch(`${API_BASE}/`);
+			data = await res.json();
+		} catch (error) {
+			console.error(error);
+		}
+	});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+{#if data}
+	<p>{data.message}</p>
+{/if}

da4c7a73f44fe976b00f0ceea598febcc75cda24
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-10-16 22:39:09 +0530

    make sveltekit run on spa mode and setup vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,36 +1,49 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { loadEnv } from 'vite';
+import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }]
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [tailwindcss(), sveltekit()],
+		server: {
+			proxy: {
+				'/api': {
+					changeOrigin: true,
+					target: env.VITE_API_BASE
 				}
 			}
-		]
-	}
+		},
+		test: {
+			expect: { requireAssertions: true },
+			projects: [
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'client',
+						environment: 'browser',
+						browser: {
+							enabled: true,
+							provider: 'playwright',
+							instances: [{ browser: 'chromium' }]
+						},
+						include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+						exclude: ['src/lib/server/**'],
+						setupFiles: ['./vitest-setup-client.ts']
+					}
+				},
+				{
+					extends: './vite.config.ts',
+					test: {
+						name: 'server',
+						environment: 'node',
+						include: ['src/**/*.{test,spec}.{js,ts}'],
+						exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					}
+				}
+			]
+		}
+	};
 });

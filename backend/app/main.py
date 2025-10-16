98d06dfc9281154659d7620d12fbf6a327088385
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-10-16 22:40:21 +0530

    get rid of middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
 
 @app.get("/api/test")
 def read_root():

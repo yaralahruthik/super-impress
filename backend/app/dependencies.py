22124a2041b53471336f90962866ac918e4656cc
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-10-20 11:27:38 +0530

    Setup database, post model and endpoints

---
+++ backend/app/dependencies.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import Session
+
+from app.database import get_session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]

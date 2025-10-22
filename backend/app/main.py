ae87dca8c3570a387fd599d79ea33d35d6ac91be
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-10-22 09:21:30 +0530

    refactor: move post endpoints to router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,8 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException
-from sqlmodel import select
+from fastapi import FastAPI
 
 from app.database import create_db_and_tables
-from app.models.post import Post
-from app.dependencies import SessionDep
+from app.routers import post
 
 
 @asynccontextmanager
@@ -16,26 +14,5 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-# Post endpoints
-@app.post("/posts/", response_model=Post)
-def create_post(post: Post, session: SessionDep):
-    session.add(post)
-    session.commit()
-    session.refresh(post)
-    return post
-
-
-@app.get("/posts/{post_id}", response_model=Post)
-def read_post(post_id: int, session: SessionDep):
-    post = session.get(Post, post_id)
-    if not post:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return post
-
-
-@app.get("/posts/", response_model=list[Post])
-def read_posts(session: SessionDep, skip: int = 0, limit: int = 20):
-    statement = select(Post).offset(skip).limit(limit)
-    posts = session.exec(statement).all()
-    return posts
+# Include routers
+app.include_router(post.router)

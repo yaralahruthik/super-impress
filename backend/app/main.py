c2006fffe75bf2b42301f7353cbf0af0d7b536de
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-11-03 20:28:15 +0530

    checkpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.auth.router import auth_router
+from app.database import create_db_and_tables
 
-# @asynccontextmanager
-# async def lifespan(_: FastAPI):
-#     create_db_and_tables()
-#     yield
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    create_db_and_tables()
+    yield
 
 
 app = FastAPI(title="Super Impress")

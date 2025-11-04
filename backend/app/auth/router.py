7bdf5b6cce47e7ffa2323fdeb18440516c2cb017
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-11-04 15:53:57 +0530

    refactor: improve error handling at auth routes

--- backend/app/auth/router.py
+++ backend/app/auth/router.py
@@ -4,14 +4,15 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
+from app.auth.config import auth_settings
 from app.auth.models import Token, User, UserCreate, UserPublic
 from app.auth.service import (
     authenticate_user,
     create_access_token,
     create_user,
     get_current_user,
+    get_user_by_email,
 )
-from app.auth.config import auth_settings
 from app.database import SessionDep
 
 auth_router = APIRouter()
@@ -19,6 +20,12 @@ auth_router = APIRouter()
 
 @auth_router.post("/register", response_model=UserPublic)
 async def register_user(user: UserCreate, session: SessionDep) -> UserPublic:
+    existing_user = get_user_by_email(session, user.email)
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_BAD_REQUEST, detail="Email already registered"
+        )
+
     db_user = create_user(session, user)
     return UserPublic.model_validate(db_user)
 
@@ -28,13 +35,20 @@ async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     session: SessionDep,
 ) -> Token:
+    if not form_data.username or not form_data.password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email and password are required",
+        )
+
     user = authenticate_user(session, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
     access_token_expires = timedelta(minutes=auth_settings.access_token_expire_minutes)
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires

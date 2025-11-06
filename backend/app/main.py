977d6b029568281734e5a2cb8ab6ecf4fec7582e
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-11-07 01:22:56 +0530

    format

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,10 @@ from app.auth.router import auth_router
 
 app = FastAPI(title="Super Impress")
 
+
 @app.get("/api/test")
 async def root():
     return {"message": "Hello World"}
 
-app.include_router(auth_router, prefix='/api')
+
+app.include_router(auth_router, prefix="/api")

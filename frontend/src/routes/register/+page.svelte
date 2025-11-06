7dcd94b37dd862d42012f33800d95c965aaf8441
yaralahruthik <yaralahruthikreddy@gmail.com>
2025-11-07 01:21:07 +0530

    simple register page

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -59,11 +59,11 @@
 	}
 </script>
 
-<main>
-	<h1>Register</h1>
+<main class="container space-y-4 p-10">
+	<h1 class="text-xl">Register</h1>
 
 	<form on:submit={handleSubmit} aria-labelledby="form-heading">
-		<fieldset disabled={isSubmitting}>
+		<fieldset class="space-y-2" disabled={isSubmitting}>
 			<legend id="form-heading">Create your account</legend>
 
 			<div>
@@ -73,6 +73,7 @@
 						type="email"
 						id="email"
 						name="email"
+						class="border"
 						bind:value={email}
 						required
 						autocomplete="email"
@@ -89,9 +90,9 @@
 						type="password"
 						id="password"
 						name="password"
+						class="border"
 						bind:value={password}
 						required
-						minlength="8"
 						autocomplete="new-password"
 						aria-required="true"
 						aria-describedby={errorMessage ? 'error-message' : undefined}
@@ -106,9 +107,9 @@
 						type="password"
 						id="confirm-password"
 						name="confirm-password"
+						class="border"
 						bind:value={confirmPassword}
 						required
-						minlength="8"
 						autocomplete="new-password"
 						aria-required="true"
 						aria-describedby={errorMessage ? 'error-message' : undefined}
@@ -116,7 +117,7 @@
 				</label>
 			</div>
 
-			<button type="submit" aria-busy={isSubmitting}>
+			<button type="submit" class="border px-2 py-1" aria-busy={isSubmitting}>
 				{isSubmitting ? 'Registering...' : 'Register'}
 			</button>
 		</fieldset>
@@ -137,6 +138,6 @@
 	<p>
 		Already have an account?
 		<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
-		<a href="/login" data-sveltekit-preload-data>Log in</a>
+		<a href="/login" class="underline" data-sveltekit-preload-data>Log in</a>
 	</p>
 </main>

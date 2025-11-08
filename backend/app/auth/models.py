ab1108494a33556e3b5406cfb741836fa6272ac6
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-11-08 09:26:23 +0530

    better password validation

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, EmailStr
+from typing import Annotated
+
+from pydantic import AfterValidator, BaseModel, EmailStr
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -23,10 +25,25 @@ class UserBase(BaseModel):
     email: EmailStr
 
 
+def password_validator(password: str) -> str:
+    """Validate password strength."""
+    if len(password) < 8 or len(password) > 15:
+        raise ValueError("Password must be between 8 and 15 characters long")
+    if not any(char.isupper() for char in password):
+        raise ValueError("Password must contain at least one uppercase letter")
+    if not any(char.islower() for char in password):
+        raise ValueError("Password must contain at least one lowercase letter")
+    if not any(char.isdigit() for char in password):
+        raise ValueError("Password must contain at least one digit")
+    if not any(char in "!@#$%^&*()-_=+[]{}|;:,.<>?/" for char in password):
+        raise ValueError("Password must contain at least one special character")
+    return password
+
+
 class UserCreate(UserBase):
     """Schema for creating a new user."""
 
-    password: str
+    password: Annotated[str, AfterValidator(password_validator)]
 
 
 class UserPublic(UserBase):

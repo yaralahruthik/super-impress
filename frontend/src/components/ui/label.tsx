ca211071a922c0bc73b3d4410b2dd38ad0ccaaa0
yaralahruthik <yaralahruthikreddy@gmail.com>
2026-01-20 19:40:30 +0530

    more deps

--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -1,21 +1,19 @@
 import * as React from 'react';
+import * as LabelPrimitive from '@radix-ui/react-label';
+
 import { cn } from '@/utils/classname';
 
-const Label = React.forwardRef<HTMLLabelElement, React.ComponentProps<'label'>>(
-	({ className, ...props }, ref) => {
-		return (
-			<label
-				ref={ref}
-				className={cn(
-					'text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
-					className
-				)}
-				{...props}
-			/>
-		);
-	}
-);
-
-Label.displayName = 'Label';
+function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
+	return (
+		<LabelPrimitive.Root
+			data-slot="label"
+			className={cn(
+				'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+				className
+			)}
+			{...props}
+		/>
+	);
+}
 
 export { Label };

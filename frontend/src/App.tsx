877d86f661f5aba7e5f61db9dfbe6e79f3e9ae56
yaralahruthik <yaralahruthikreddy@gmail.com>
2026-01-20 13:36:18 +0530

    providers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider as JotaiProvider } from 'jotai';
+
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<>
-			<div>hello</div>
-		</>
+		<JotaiProvider>
+			<QueryClientProvider client={queryClient}>
+				<div>hello</div>
+			</QueryClientProvider>
+		</JotaiProvider>
 	);
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { Provider as JotaiProvider } from 'jotai';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
@@ -6,8 +5,6 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<JotaiProvider>
-			<App />
-		</JotaiProvider>
+		<App />
 	</StrictMode>
 );

49486db58e185cd9f98993e5fea8ead86f498a78
yaralahruthik <yaralahruthikreddy@gmail.com>
2026-01-27 12:03:53 +0530

    better auth handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
+import { auth } from "./auth";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().mount(auth.handler).listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
